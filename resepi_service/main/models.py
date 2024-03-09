7e419e194205688ab4b5e039bda1ff3ee1fd37e6
Karina534 <smirnova7.04@mail.ru>
2024-03-09 13:31:52 +0300

    made my first table

--- resepi_service/main/models.py
+++ resepi_service/main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    Name = models.CharField(max_length=30, blank=False)
+    Sername = models.CharField(max_length=30, blank=False)
+    email = models.EmailField()
+    content = models.TextField(blank=True)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    time_create = models.DateTimeField(auto_now_add=True)
+    is_published = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.Name}, {self.Sername}'
